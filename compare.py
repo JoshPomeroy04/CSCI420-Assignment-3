acb30193f35f145d53836e2a27215ece6f77d492
Josh Pomeroy <joshuaming2004@gmail.com>
2025-04-23 19:07:43 -0400

    Calculates and saves intermodel response scores

--- compare.py
+++ compare.py
@@ -24,6 +24,10 @@ gemini_responses = {}
 gemini_scores = {}
 gpt_responses = {}
 gpt_scores = {}
+gpt_gemini_R1 = {}
+gpt_gemini_R1_scores = {}
+gpt_gemini_R2 = {}
+gpt_gemini_R2_scores = {}
 
 def read_json_files(folder_path):
     """
@@ -103,12 +107,63 @@ def save_to_csv(results, file):
         for key in results.keys():
              csv_writer.writerow([key, results[key]])
 
+def calc_codebleu_scores_intermodel(model1, model2, response, keys, language, dict):
+    """
+    Calculates codebleu scores for a given list of keys for a specified language
+
+    Args:
+        model1 (dict): Dictionary containing all results to calculate from first model's responses
+        model2 (dict): Dictionary containing all results to calculate from second model's responses
+        response (str): Either R1 or R2 to indicate which responses to compare
+        keys (list): List of keys to calculate scores for
+        language (str): Language that the values of the keys are in
+        dict (dict): Dictionary to store results in
+
+    Returns:
+        None
+    """
+    for key in keys:
+        reference = model1[key][response]
+        hypothesis = model2[key][response]
+        codebleuscore = calc_codebleu([reference], [hypothesis], lang=language, weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
+        dict[key] = round(codebleuscore["codebleu"] * 100, 2)
+
+def calc_all_intermodel(model1, model2, response, dict):
+    """
+    Calculates all BLEU scores for a models outputs
+
+    Args:
+        model1 (dict): Dictionary containing all results to calculate from first model's responses
+        model2 (dict): Dictionary containing all results to calculate from second model's responses
+        response (str): Either R1 or R2 to indicate which responses to compare
+        dict (dict): Dictionary to store results in
+
+    Returns:
+        None
+    """
+    for key in BLEU_KEYS:
+        reference = model1[key][response]
+        hypothesis = model2[key][response]
+        bleuscore = bleu.sentence_score(hypothesis, [reference]).score
+        dict[key] = round(bleuscore, 2)
+        
+
+    calc_codebleu_scores_intermodel(model1, model2, response, PYTHON_KEYS, "python", dict)
+    calc_codebleu_scores_intermodel(model1, model2, response, JAVA_KEYS, "java", dict)
+    calc_codebleu_scores_intermodel(model1, model2, response, CPP_KEYS, "cpp", dict)
+    calc_codebleu_scores_intermodel(model1, model2, response, JS_KEYS, "javascript", dict)
+
 gemini_responses = read_json_files(f"{FOLDER_PATH}/data/Gemini Responses/")
 gpt_responses = read_json_files(f"{FOLDER_PATH}/data/GPT Responses/")
 
+
 calc_all(gemini_responses, gemini_scores)
 calc_all(gpt_responses, gpt_scores)
+calc_all_intermodel(gemini_responses, gpt_responses, "R1", gpt_gemini_R1_scores)
+calc_all_intermodel(gemini_responses, gpt_responses, "R2", gpt_gemini_R2_scores)
 
 save_to_csv(gemini_scores, "gemini.csv")
 save_to_csv(gpt_scores, "gpt.csv")
+save_to_csv(gpt_gemini_R1_scores, "gpt-gemini-R1.csv")
+save_to_csv(gpt_gemini_R2_scores, "gpt-gemini-R2.csv")
 
