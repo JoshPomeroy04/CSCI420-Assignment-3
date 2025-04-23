0fb402283b31e207b71f2b10ec512b2c8bf2fa82
Josh Pomeroy <joshuaming2004@gmail.com>
2025-04-23 18:51:13 -0400

    Calculates BLEU or CodeBLEU score based on the different prompting techniques

---
+++ compare.py
@@ -0,0 +1,85 @@
+import os
+import nltk
+import json
+from sacrebleu.metrics import BLEU
+from codebleu import calc_codebleu
+
+nltk.download('punkt_tab')
+
+FOLDER_PATH = "CSCI_420/CSCI420-Assignment-3"
+BLEU_KEYS = ["BugClassificationC++.json", "CodeSummarizationJava.json", "FunctionSummarizationPython.json", 
+             "NullDereferenceDetectionJava.json", "PromptfromCodeCommentsPython.json", 
+             "SummaryDecompositionC++.json", "SQLSchemaDesignSQL.json", "DataClasstoAPIConversionKotlin.json"]
+PYTHON_KEYS = ["BugFixingPython-Off-by-One.json", "ConstructorCompletionPython.json", 
+               "CSVParserVariantsPython.json", "FixingFactorialBugPython.json",
+               "GeneratingEmailValidatorsPython+Regex.json", "GeneratingFlaskAPIsPython.json",
+               "PurposeInferenceCompletionPython.json", "RecursiveFunctionCompletionPython.json"]
+JAVA_KEYS = ["BinarySearchCompletionJava.json"]
+CPP_KEYS = ["Self-ConsistencyBugFixingC++.json", "LinkedListNodeDeletionC.json"]
+JS_KEYS = ["PromptChainingBugIdentificationFixJavaScript.json"]
+
+bleu = BLEU()
+gemini_responses = {}
+gemini_scores = {}
+gpt_responses = {}
+gpt_scores = {}
+
+def read_json_files(folder_path):
+    """
+    Reads a folder containing JSON files
+
+    Args:
+        folder_path (str): The path to the folder containing JSON files
+
+    Returns:
+        dict: The data from the JSON files, as a Python dictionary.
+    """
+    data = {}
+    for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as file:
+                    data[filename] = json.load(file)
+
+    return data
+
+def calc_codebleu_scores(model, keys, language, dict):
+    """
+    Calculates codebleu scores for a given list of keys for a specified language
+
+    Args:
+        model (dict): Dictionary containing all results to calculate
+        keys (list): List of keys to calculate scores for
+        language (str): Language that the values of the keys are in
+        dict (dict): Dictionary to store results in
+
+    Returns:
+        None
+    """
+    for key in keys:
+        reference = model[key]["R1"]
+        hypothesis = model[key]["R2"]
+        codebleuscore = calc_codebleu([reference], [hypothesis], lang=language, weights=(0.25, 0.25, 0.25, 0.25), tokenizer=None)
+        dict[key] = round(codebleuscore["codebleu"] * 100, 2)
+
+def calc_all(model, dict):
+    for key in BLEU_KEYS:
+        reference = model[key]["R1"]
+        hypothesis = model[key]["R2"]
+        bleuscore = bleu.sentence_score(hypothesis, [reference]).score
+        dict[key] = round(bleuscore, 2)
+        
+
+    calc_codebleu_scores(model, PYTHON_KEYS, "python", dict)
+    calc_codebleu_scores(model, JAVA_KEYS, "java", dict)
+    calc_codebleu_scores(model, CPP_KEYS, "cpp", dict)
+    calc_codebleu_scores(model, JS_KEYS, "javascript", dict)
+
+gemini_responses = read_json_files(f"{FOLDER_PATH}/data/Gemini Responses/")
+gpt_responses = read_json_files(f"{FOLDER_PATH}/data/GPT Responses/")
+
+calc_all(gemini_responses, gemini_scores)
+calc_all(gpt_responses, gpt_scores)
+
+print(gemini_scores)
+print(gpt_scores)
